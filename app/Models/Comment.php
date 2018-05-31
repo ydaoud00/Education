d3e108456209b57f4e2cfdd4e59cc162c940b05c
ydaoud00 <ydaoud00@estudiantes.unileon.es>
2018-05-31 20:39:02 +0200

    modelo y migracion de comentarios

---
+++ app/Models/Comment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $table = 'comments';
+	protected $fillable = ['description', 'file_id'];
+  	protected $guarded = ['id'];
+
+  	public function file()
+    {
+        return $this->belongsTo('App\Models\File');
+    }
+}
